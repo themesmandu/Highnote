ce475d955c95daf65d7a9bee887dc55d5d68a87c
sauravxtha <sauravxtha@gmail.com>
2021-02-19 14:14:20 +0545

    add navbar class

--- template-parts/navigation/main-navbar.php
+++ template-parts/navigation/main-navbar.php
@@ -11,9 +11,9 @@
 ?>
 <?php if ( get_theme_mod( 'mainmenu_dropdown_mode' ) !== 'bootstrap' ) { ?>
 
-	<nav class="navbar navbar-expand-lg main-navigation appear-left<?php echo esc_attr( ( get_theme_mod( 'menubar_absolute' ) ) ? ' navbar-above' : '' ); ?>">
+	<nav class="navbar navbar-expand-lg main-navigation appear-left<?php echo esc_attr( ( get_theme_mod( 'menubar_absolute' ) ) ? ' navbar-above' : '' ); ?><?php echo esc_attr( ( get_theme_mod( 'mainmenu_style' ) === 'fixed' ) ? ' navbar-fixed' : '' ); ?>">
 	<?php } else { ?>
-		<nav class="navbar navbar-expand-lg main-navigation<?php echo esc_attr( ( get_theme_mod( 'menubar_absolute' ) ) ? ' navbar-above' : '' ); ?>">
+		<nav class="navbar navbar-expand-lg main-navigation<?php echo esc_attr( ( get_theme_mod( 'menubar_absolute' ) ) ? ' navbar-above' : '' ); ?><?php echo esc_attr( ( get_theme_mod( 'mainmenu_style' ) === 'fixed' ) ? ' navbar-fixed' : '' ); ?>">
 		<?php } ?>
 		<div class="container">
 			<?php if ( ! has_custom_logo() ) { ?>
