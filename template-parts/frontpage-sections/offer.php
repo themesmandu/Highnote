3254722876a6f7fbca6450a115b988cb03772cca
sauravxtha <sauravxtha@gmail.com>
2021-02-23 11:28:08 +0545

    update coding error

--- template-parts/frontpage-sections/offer.php
+++ template-parts/frontpage-sections/offer.php
@@ -8,57 +8,11 @@
 ?>
 <?php if ( get_theme_mod( 'offer_title' ) || get_theme_mod( 'offer_desc' ) ) : ?>
 <section class="section_offer section_eight">
-<<<<<<< HEAD
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <?php if ( get_theme_mod( 'offer_title' ) || get_theme_mod( 'offer_desc' ) ) : ?>
-            <div class="column col-md-6">
-                <div class="section_header">
-                    <?php if ( get_theme_mod( 'offer_title' ) ) : ?>
-                    <h2 class="section-heading"><?php echo wp_kses_post( get_theme_mod( 'offer_title' ) ); ?></h2>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod( 'offer_desc' ) ) : ?>
-                    <h3 class="sec-description"><?php echo wp_kses_post( get_theme_mod( 'offer_desc' ) ); ?></h3>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <?php endif; ?>
-
-            <?php if ( get_theme_mod( 'offer_sub_section_title' ) || get_theme_mod( 'offer_ribbon_label' ) || get_theme_mod( 'offer_sub_section_sub_title' ) || get_theme_mod( 'offer_sub_section_desc' ) ) : ?>
-            <div class="column col-md-6">
-                <div class="column-content">
-                    <?php if ( get_theme_mod( 'offer_sub_section_title' ) || get_theme_mod( 'offer_ribbon_label' ) ) : ?>
-                    <div class="offer-wrap">
-                        <?php if ( get_theme_mod( 'offer_sub_section_title' ) ) : ?>
-                        <h4 class="offer-headline">
-                            <?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_title' ) ); ?></h4>
-                        <?php endif; ?>
-                        <?php if ( get_theme_mod( 'offer_ribbon_label' ) ) : ?>
-                        <span class="ribbon"><?php echo wp_kses_post( get_theme_mod( 'offer_ribbon_label' ) ); ?></span>
-                        <?php endif; ?>
-                    </div>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod( 'offer_sub_section_sub_title' ) ) : ?>
-                    <h5 class="offer-detail">
-                        <?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_sub_title' ) ); ?></h5>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod( 'offer_sub_section_desc' ) ) : ?>
-                    <p class="offer-description">
-                        <?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_desc' ) ); ?></p>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <?php endif; ?>
-        </div>
-    </div>
-</section>
-=======
 	<div class="overlay"></div>
 	<div class="container">
 		<div class="row">
 			<?php if ( get_theme_mod( 'offer_title' ) || get_theme_mod( 'offer_desc' ) ) : ?>
-			<div class="column header-column">
+			<div class="column col-md-6">
 				<div class="section_header">
 					<?php if ( get_theme_mod( 'offer_title' ) ) : ?>
 					<h2 class="section-heading"><?php echo wp_kses_post( get_theme_mod( 'offer_title' ) ); ?></h2>
@@ -71,22 +25,22 @@
 			<?php endif; ?>
 
 			<?php if ( get_theme_mod( 'offer_sub_section_title' ) || get_theme_mod( 'offer_ribbon_label' ) || get_theme_mod( 'offer_sub_section_sub_title' ) || get_theme_mod( 'offer_sub_section_desc' ) ) : ?>
-			<div class="column content-column">
+			<div class="column col-md-6">
 				<div class="column-content">
 					<?php if ( get_theme_mod( 'offer_sub_section_title' ) || get_theme_mod( 'offer_ribbon_label' ) ) : ?>
 					<div class="offer-wrap">
 						<?php if ( get_theme_mod( 'offer_sub_section_title' ) ) : ?>
-						<h5 class="offer-headline">
-							<?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_title' ) ); ?></h5>
+						<h4 class="offer-headline">
+							<?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_title' ) ); ?></h4>
 						<?php endif; ?>
 						<?php if ( get_theme_mod( 'offer_ribbon_label' ) ) : ?>
-						<h6 class="ribbon"><?php echo wp_kses_post( get_theme_mod( 'offer_ribbon_label' ) ); ?></h6>
+						<span class="ribbon"><?php echo wp_kses_post( get_theme_mod( 'offer_ribbon_label' ) ); ?></span>
 						<?php endif; ?>
 					</div>
 					<?php endif; ?>
 					<?php if ( get_theme_mod( 'offer_sub_section_sub_title' ) ) : ?>
-					<h6 class="offer-detail">
-						<?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_sub_title' ) ); ?></h6>
+					<h5 class="offer-detail">
+						<?php echo wp_kses_post( get_theme_mod( 'offer_sub_section_sub_title' ) ); ?></h5>
 					<?php endif; ?>
 					<?php if ( get_theme_mod( 'offer_sub_section_desc' ) ) : ?>
 					<p class="offer-description">
@@ -99,4 +53,3 @@
 	</div>
 </section>
 <?php endif; ?>
->>>>>>> c9df8aa2be6815db96d7a1055b1863ff35e6778e
