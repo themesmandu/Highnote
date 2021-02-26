ddff6c823c426369e3896db45ff3335a11e808ad
sauravxtha <sauravxtha@gmail.com>
2021-02-26 13:31:58 +0545

    update dynamic styles

--- inc/dynamic-styles.php
+++ inc/dynamic-styles.php
@@ -71,9 +71,10 @@ function highnote_typography_mediascreen_css() {
 <!--Header background CSS-->
 <style type="text/css">
 @media (max-width: 991px) {
-	h1 {
-		font-size: <?php echo esc_html( get_theme_mod( 'typography_headings_h1_tablet' ) ); ?>;
-	}
+		h1 {font-size: <?php echo esc_html( get_theme_mod( 'typography_headings_h1_tablet' )[ array( 'font-size' ) ] ); ?>;line-height: <?php echo esc_html( get_theme_mod( 'typography_headings_h1_tablet' )[ array( 'line-height' ) ] ); ?>;}
+		h2{
+
+		}
 }
 
 </style>
