569fa0eb7b25765777f25cb7ce77f6fa7028a2ae
sauravxtha <sauravxtha@gmail.com>
2021-02-27 12:03:34 +0545

    change name

--- assets/admin/customizer/js/post-page-setting.js
+++ assets/admin/customizer/js/post-page-setting.js
@@ -35,15 +35,15 @@ function toggleLayoutSettings(setting) {
 }
 
 function showStandardLayoutOptions() {
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_sandard').fadeIn(1000);
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_sandard').fadeIn(1000);
     jQuery('#customize-control-entry_footer_bgcolor').fadeIn(1000);
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_list').hide();
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_list2').hide();
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_list').hide();
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_list2').hide();
 }
 
 function showListLayoutOptions() {
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_sandard').hide();
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_sandard').hide();
     jQuery('#customize-control-entry_footer_bgcolor').hide();
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_list').fadeIn(1000);
-    jQuery('#customize-control-beatsmandu_theme_options-blog_sortable_content_list2').fadeIn(1000);
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_list').fadeIn(1000);
+    jQuery('#customize-control-highnote_theme_options-blog_sortable_content_list2').fadeIn(1000);
 }
