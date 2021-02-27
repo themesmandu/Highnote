6c6107d48f7f616b1155d81ed30e4e458c532656
sauravxtha <sauravxtha@gmail.com>
2021-02-27 15:50:08 +0545

    fix single page navigation

--- single.php
+++ single.php
@@ -26,9 +26,52 @@ get_header();
 			 * called content-single-___.php (where ___ is the Post Format name) and that will be used instead.
 			 */
 			get_template_part( 'template-parts/post/single', get_post_format() );
+			?>
+			<?php if ( ! is_attachment() ) { ?>
+				<nav class="navigation card-footer">
+					<div class="nav_direction">
+						<?php
+						$prev_post = get_previous_post();
+						if ( $prev_post ) {
+							?>
+						<div class="previous_post column">
+							<?php $prevthumbnail = get_the_post_thumbnail_url( $prev_post->ID, 'prev-next-link-image' ); ?>
+							<?php if ( $prevthumbnail ) : ?>
+							<figure>
+								<a href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>"><img
+										src="<?php echo esc_url( $prevthumbnail ); ?>" alt="<?php echo esc_html__( 'Previous Post Thumbnail', 'highnote' ); ?>"></a>
+							</figure>
+							<?php endif; ?>
+							<div class="prev_title">
+								<span><?php echo esc_html__( 'Prev Post', 'highnote' ); ?></span>
+								<?php previous_post_link( '%link', "<div class='detials'><span>%title</span></div>" ); ?>
+							</div>
+						</div>
 
-			the_post_navigation();
+							<?php
+						}
+							$next_post = get_next_post();
+						if ( $next_post ) {
+							?>
+						<div class="next_post column">
+							<?php $nextthumbnail = get_the_post_thumbnail_url( $next_post->ID, 'prev-next-link-image' ); ?>
+							<?php if ( $nextthumbnail ) : ?>
+							<figure>
+								<a href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>"><img
+										src="<?php echo esc_url( $nextthumbnail ); ?>" alt="<?php echo esc_html__( 'Next Post Thumbnail', 'highnote' ); ?>"></a>
+							</figure>
+							<?php endif; ?>
 
+							<div class="next_title">
+								<span><?php echo esc_html__( 'Next Post', 'highnote' ); ?></span>
+								<?php next_post_link( '%link', "<div class='detials'><span>%title</span></div>" ); ?>
+							</div>
+						</div>
+						<?php } ?>
+					</div>
+				</nav>
+				<?php } ?>
+			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
