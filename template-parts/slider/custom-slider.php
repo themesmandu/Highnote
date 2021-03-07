e58c661bd937bde758ec5405f766e61a0ad782ea
Rupesh Rai <51738506+rupeshchamling@users.noreply.github.com>
2021-03-08 01:54:32 +0545

    remove list style marker from image slider

--- template-parts/slider/custom-slider.php
+++ template-parts/slider/custom-slider.php
@@ -15,7 +15,8 @@ if ( is_front_page() && ! is_home() && get_theme_mod( 'slider_toggle' ) ) : ?>
 	<div class="slide-img-wrap">
 		<ul class="slider-list">
 		<?php foreach ( $active_sliders as $active_slider ) : ?>
-			<li><img src="<?php echo esc_url( wp_get_attachment_url( $active_slider['slider_img'] ) ); ?>" alt="<?php echo esc_url( wp_get_attachment_url( $active_slider['slider_img'] ) ); ?>" title="<?php echo esc_html( $active_slider['slider_heading'] ); ?>" />
+			<li>
+				<img src="<?php echo esc_url( wp_get_attachment_url( $active_slider['slider_img'] ) ); ?>" alt="<?php echo esc_url( wp_get_attachment_url( $active_slider['slider_img'] ) ); ?>" title="<?php echo esc_html( $active_slider['slider_heading'] ); ?>" />
 				<?php if ( $active_slider['slider_subheading'] ) : ?>
 				<h2 class="beats-sub-heading from-bottom"><?php echo esc_html( $active_slider['slider_subheading'] ); ?></h2>
 				<?php endif; ?>
