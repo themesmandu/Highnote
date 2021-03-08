2c0cc99f5589af8b723ab2751bff1ded403bab98
sauravxtha <sauravxtha@gmail.com>
2021-03-08 12:49:44 +0545

    add bypostauthor class

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -69,7 +69,7 @@ if ( ! function_exists( 'highnote_posted_by' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'by %s', 'post author', 'highnote' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard"><a class="url fn n bypostauthor" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
